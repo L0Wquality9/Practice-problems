3940c0ba17a18358b1d70c0422f7eb29f52526d1
L0Wquality9 <nicduclos9@gmail.com>
2024-11-23 17:43:19 -0500

    continued trying part 3

--- Prelim2/part3/part_3.py
+++ Prelim2/part3/part_3.py
@@ -19,7 +19,54 @@ def part_3(words):
 
     ### You code goes here ###
     ### Votre code va ici ###
-
+    grid = []
+    word_1 = words[0]
+    word_2 = words[1]
+    for i in range(len(word_2) + 2):
+        grid.append([])
+    
+        for t in range(len(word_1) + 2):
+            
+            grid[i].append(0)
+    grid[1][1] = 0
+    for i in range(len(grid)-2):
+        grid[i+2][0]=word_2[i]
+    for i in range(len(word_1)):
+        grid[0][i+2] = word_1[i]
+    for i in grid:
+        print(*i)
+    
+    for i in range(len(word_1)):
+        grid[1][i+2] = grid[1][i+1]+1
+    for i in range(len(word_2)):
+        grid[i+2][1] = grid[i+1][1]+1
+    string = []
+    for i in range(len(word_1)+2):
+        string.append("-")
+    print(*string)
+    if len(word_1)>len(word_2):
+        
+        for t in range(len(word_2)):
+            for i in range(len(word_1)):
+                if i+1>len(word_2):
+                    grid[t+2][i+2] = grid[t+2][i+1]+1
+                elif word_1[i]!=word_2[t]:
+                    grid[t+2][i+2] = grid[t+1][i+1]+1
+                elif word_1[i]==word_2[i]:
+                    grid[t+2][i+2] = grid[t+1][i+1]
+            for p in grid:
+                print(*p)
+            print(*string)
+        
+        
+    else:
+        pass
+    
+            
+    for i in grid:
+        print(*i)
+    
 
 
     return CAPTCHA
+part_3(words=["sitting", "kitten"])
