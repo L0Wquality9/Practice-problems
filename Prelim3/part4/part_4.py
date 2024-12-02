68dd69639b097de065d0a5d263e8cfa6be82fc00
L0Wquality9 <nicduclos9@gmail.com>
2024-12-02 12:27:04 -0500

    made most of part 4

--- Prelim3/part4/part_4.py
+++ Prelim3/part4/part_4.py
@@ -21,8 +21,38 @@ def part_4(symptomes, prescriptions, diagnostics, medicaments):
     diagnostic = ""
     ### You code goes here ###
     ### Votre code va ici ###
-
+    for i in prescriptions:
+        #print(i)
+        for j in medicaments:
+            #print(j[0])
+            if i == j[0]:
+                for k in symptomes:
+                    if k in j[1]:
+                        symptomes.remove(k)
+    for i in diagnostics:
+        if all(elements in i[1] for elements in symptomes):
+            diagnostic = i[0]
+            
 
 
 
     return diagnostic
+diagnostics=[["Diabète", ["Déshydratation", "Gain de poids", "Perte de poids", "Besoin d'uriner", "Fatigue", "Troubles de vision", "Infections", "Picotements/Engourdissements des extrémités"]],
+             ["COVID-19", ["Maux de gorge", "Écoulement nasal", "Éternuements", "Toux", "Fièvre", "Difficultés respiratoires", "Frissons", "Fatigue", "Maux de tête", "Douleurs musculaires", "Perte de l'odorat/du goût"]],
+             ["Grippe", ["Toux", "Fièvre", "Douleurs musculaires", "Frissons", "Fatigue", "Maux de gorge", "Maux de tête", "Perte d'appétit", "Écoulement nasal", "Congestion nasale"]],
+             ["Rhume", ["Toux", "Fatigue", "Éternuements", "Écoulement nasal", "Congestion nasale", "Maux de gorge"]],
+             ["Sclérose en plaques", ["Dépression", "Douleurs", "Faiblesse", "Fatigue", "Intolérance à la chaleur", "Troubles de vision", "Troubles d'équilibre"]],
+             ["Syphilis", ["Maux de tête", "Étourdissements", "Troubles de vision", "Troubles d'équilibre", "Troubles de coordination", "Démence", "Perte auditive"]],
+             ["Gastroentérite", ["Diarrhée", "Douleurs abdominales", "Nausées", "Vomissements", "Fièvre", "Maux de tête", "Douleurs musculaires", "Perte d'appétit"]],
+             ["Mononucléose", ["Fièvre", "Frissons", "Fatigue", "Perte d'appétit", "Douleurs musculaires", "Maux de tête", "Difficultés à avaler"]],
+             ["Anémie", ["Pâleur", "Essoufflements", "Fatigue", "Palpitations", "Maux de tête", "Étourdissements", "Faiblesse", "Difficultés à se concentrer"]],
+             ["Tuberculose", ["Toux", "Douleurs thoraciques", "Faiblesse", "Fatigue", "Perte de poids", "Perte d'appétit", "Frissons", "Fièvre", "Sueurs nocturnes"]],
+             ["Maladie coeliaque", ["Douleurs abdominales", "Diarrhée", "Constipation", "Fatigue", "Dépression"]]
+            ]
+
+medicaments=[["Fluvoxamine", ["Nausées", "Insomnie", "Somnolence", "Céphalées", "Étourdissements", "Constipation", "Sécheresse de la bouche", "Vomissements"]],
+             ["Antibiotiques", ["Diarrhée", "Fièvre", "Douleurs abdominales", "Perte d'appétit", "Déshydratation", "Nausées"]],
+             ["Acétaminophène", ["Sécheresse de la bouche", "Maux de tête", "Étourdissements"]],
+             ["Ibuprofène", ["Démangeaisons", "Insomnie", "Nausées", "Vomissements", "Douleurs/Crampes d'estomac", "Diarrhée", "Fatigue", "Indigestion", "Constipation", "Ballonnements"]]
+            ]
+part_4(["Troubles de vision", "Étourdissements", "Maux de tête", "Perte de poids"], ["Acétaminophène"], diagnostics, medicaments)
