f44fdda808229714b5d41423179bee128e9f56f7
L0Wquality9 <nicduclos9@gmail.com>
2024-12-02 12:06:13 -0500

    Added in prelim 3

---
+++ Prelim3/part2/part_2.py
@@ -0,0 +1,25 @@
+# For your imports look at the rulebook to see what is allowed
+# Pour les import permis allez vous référer au livre des règlements
+"""part_2.py
+This is the template file for the part 2 of the Prelim 3.
+Ceci est le fichier template pour la partie 2 du Prelim 3.
+"""
+
+def part_2(arrivals: int, mu: int, c: int) -> float:
+    """
+    Computes the cost of custom made top hats
+    
+    Parameters:
+        arrivals int: the width of the opening of the hat
+        mu int: the height of the hat
+        c int: the width of the brim
+
+    Returns:
+        float: temps d'attente en minutes
+    """
+    wait_time_minutes = 0
+    ### You code goes here ###
+    ### Votre code va ici ###
+    
+
+    return wait_time_minutes
