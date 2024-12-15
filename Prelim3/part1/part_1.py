8fcbbd21075b8d72a4ada41a68572ea678400660
L0Wquality9 <nicduclos9@gmail.com>
2024-12-15 17:47:07 -0500

    Im done for the day.
    This is too hard. Please do this for me

--- Prelim3/part1/part_1.py
+++ Prelim3/part1/part_1.py
@@ -18,61 +18,64 @@ def part_1(genes) -> int:
     probability = 0.0
     ### You code goes here ###
     ### Votre code va ici ###
-    tree = []
     rows = is_tree(len(genes))+1
-    genes2 = genes[1:]
-    print(genes2)
-    for i in range(rows-1):
-        tree.append([])
+    tree = create_tree(genes, rows)
     
-    tree[0].append(genes[0])
-    
-    for i in range(1,rows-1):
-        print(tree)
-        print(i)
-        for j in range(len(tree[i-1])*2):
-            tree[i].append(genes2[0])
-            genes2.pop(0)
     for i in range(rows):
         u = int(((len(tree[-i])/2)))
         for j in range(u):
-            if tree[-i][j*2] == "XX":
-                print(tree[-i][j])
-                if tree[-i][j*2+1].isupper():
-                    print(tree[-i][j*2+1])
-                elif tree[-i][j*2+1].islower():
-                    print(tree[-i][j*2+1])
-                else:
-                    print(tree[-i][j*2+1])
+            
+                if isinstance(tree[-i][j*2], int or float):
+                    pass
                 
-            elif tree[-i] == tree[0]:
-                break
-            else:
-                if tree[-i][j*2].isupper():
+            
+                
+                elif tree[-i][j*2] == "XX":
+                    print(tree[-i][j])
+                    if tree[-i][j*2+1].isupper():
+                        print(tree[-i][j*2+1])
+                    elif tree[-i][j*2+1].islower():
+                        print(tree[-i][j*2+1])
+                    else:
+                        print(tree[-i][j*2+1])
                     
-                    print(tree[-i][j*2])
-                    if tree[-i][j*2+1].isupper():
-                        print(tree[-i][j*2+1])
-                    elif tree[-i][j*2+1].islower():
-                        print(tree[-i][j*2+1])
-                    else:
-                        print(tree[-i][j*2+1])
-                elif tree[-i][j*2].islower():
-                    print(tree[-i][j*2])
-                    if tree[-i][j*2+1].isupper():
-                        print(tree[-i][j*2+1])
-                    elif tree[-i][j*2+1].islower():
-                        print(tree[-i][j*2+1])
-                    else:
-                        print(tree[-i][j*2+1])
+                elif tree[-i] == tree[0]:
+                    break
                 else:
-                    print(tree[-i][j*2])
-                    if tree[-i][j*2+1].isupper():
-                        print(tree[-i][j*2+1])
-                    elif tree[-i][j*2+1].islower():
-                        print(tree[-i][j*2+1])
+                    if tree[-i][j*2].isupper():
+                        
+                        print(tree[-i][j*2])
+                        if tree[-i][j*2+1].isupper():
+                            tree = bothUpper(tree, i, j)
+                            print(tree[-i][j*2+1])
+                        elif tree[-i][j*2+1].islower():
+                            tree = upperLower(tree, i, j)
+                            print(tree[-i][j*2+1])
+                        else:
+                            tree = case4(tree, i, j)
+                            print(tree[-i][j*2+1])
+                    elif tree[-i][j*2].islower():
+                        print(tree[-i][j*2])
+                        if tree[-i][j*2+1].isupper():
+                            upperLower(tree, i, j)
+                            print(tree[-i][j*2+1])
+                        elif tree[-i][j*2+1].islower():
+                            bothLower(tree, i, j)
+                            print(tree[-i][j*2+1])
+                        else:
+                            case3(tree, i, j)
+                            print(tree[-i][j*2+1])
                     else:
-                        print(tree[-i][j*2+1])
+                        print(tree[-i][j*2])
+                        if tree[-i][j*2+1].isupper():
+                            print(tree[-i][j*2+1])
+                            case4(tree, i, j)
+                        elif tree[-i][j*2+1].islower():
+                            print(tree[-i][j*2+1])
+                            case3(tree, i, j)
+                        else:
+                            upperLower(tree, i, j)
+                            print(tree[-i][j*2+1])
                 
                 
             
@@ -85,7 +88,8 @@ def part_1(genes) -> int:
         
             
         
-    print(tree)
+    for i in tree:
+        print(*i)
     
             
 
@@ -111,8 +115,42 @@ def is_tree(xValue):
             rows += 1
     return rows+1
 
-
+def create_tree(genes, rows):
+    tree = []
+    
+    genes2 = genes[1:]
+    print(genes2)
+    for i in range(rows-1):
+        tree.append([])
+    
+    tree[0].append(genes[0])
+    
+    for i in range(1,rows-1):
+        print(tree)
+        print(i)
+        for j in range(len(tree[i-1])*2):
+            tree[i].append(genes2[0])
+            genes2.pop(0)
+    return tree
+def bothUpper(tree, i, j):
+    tree[-i-1][j] = 0
+    print(tree)
+    return tree
+def bothLower(tree, i, j):
+    tree[-i-1][j] = 100
+    return tree
+#case 3 is 1 all lower and one both
+def case3(tree, i, j):
+    tree[-i-1][j] = 12.5
+    return tree
+#case 4 is 1 all upper and one both
+def case4(tree, i, j):
+    tree[-i-1][j] = 25
+    return tree
+def upperLower(tree, i, j):
+    tree[-i-1][j] = 25
+    return tree
     
 
 
-part_1(["XX", "XX", "XX", "XX", "Tt", "tt", "XX", "TT", "TT", "Tt", "Tt", "Tt", "XX", "TT", "tt"])
+part_1(["XX", "Cc", "cc"])
